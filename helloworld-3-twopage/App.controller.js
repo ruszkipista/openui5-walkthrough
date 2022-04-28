1e3918cfb454711f7b59772a253415b98222b145
ruszkipista <oi5672@gmail.com>
2022-04-28 12:04:15 +0100

    Step 6: Modules - add MessageToast module

--- helloworld-3-twopage/App.controller.js
+++ helloworld-3-twopage/App.controller.js
@@ -1,8 +1,16 @@
-sap.ui.define([
-	"sap/ui/core/mvc/Controller",
+// We extend the array of required modules with the fully qualified path to sap/ui/model/json/JSONModel. 
+// Once all modules are loaded, the callback function is called (2nd parameter)
+// and we can make use of both objects by accessing the parameters passed on to the function.
+// This Asynchronous Module Definition (AMD) syntax allows to clearly separate the module loading
+// from the code execution and greatly improves the performance of the application. 
+// The browser can decide when and how the resources are loaded prior to code execution.
+sap.ui.define(
+    // array of required modules
+    ["sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel"
-    ], 
+	"sap/ui/model/json/JSONModel"],
+    // call back function for after modules loaded
+    // Use the name of the artifact to load for naming the function parameters (without namespace)
     function (Controller, MessageToast, JSONModel) {
         "use strict";
 
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,21 @@
+// We extend the array of required modules with the fully qualified path to sap.m.MessageToast. 
+// Once both modules, Controller and MessageToast, are loaded, the callback function is called
+// and we can make use of both objects by accessing the parameters passed on to the function.
+// This Asynchronous Module Definition (AMD) syntax allows to clearly separate the module loading
+// from the code execution and greatly improves the performance of the application. 
+// The browser can decide when and how the resources are loaded prior to code execution.
 sap.ui.define(
-    ["sap/ui/core/mvc/Controller"],
-    function (Controller) {
+    // array of required modules
+    ["sap/ui/core/mvc/Controller",
+     "sap/m/MessageToast"],
+    // call back function for after modules loaded
+    // Use the name of the artifact to load for naming the function parameters (without namespace)     
+    function (Controller, MessageToast) {
       "use strict";
       return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
           onShowHello: function(){
             // show a native JavaScript alert
-            alert("Hello World");
+            MessageToast.show("Hello UI5!");
           }
       });
  });
