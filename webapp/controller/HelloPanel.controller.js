dee9afbb912c745131d29a9fb3a9283b0be0895a
ruszkipista <oi5672@gmail.com>
2022-05-02 21:56:35 +0100

    Step 17: Fragment Callbacks

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -6,7 +6,7 @@ sap.ui.define(
       "use strict";
 
       return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-          onShowToast: function(){
+          onPressShowToast: function(){
             // read msg from i18n model
             let oBundle = this.getView().getModel("i18n").getResourceBundle();
             let sRecipient = this.getView().getModel().getProperty("/recipient/name");
@@ -15,24 +15,21 @@ sap.ui.define(
             MessageToast.show(sMsg);
           },
 
-          onOpenDialog: function(){
+          onPressOpenDialog: function(){
             // create dialog lazily
             if (!this.pDialog) {
               this.pDialog = this.loadFragment({
                 name: "sap.ui.demo.walkthrough.view.HelloDialog"
               });
             }
-            this.pDialog
-            .then(function(oDialog) {
-              oDialog.open();
-            });
+            this.pDialog.then(oDialog => oDialog.open());
           },
 
-          onPressDialogCancelButton: function(){
-            this.pDialog
-            .then(function(oDialog) {
-              oDialog.close();
-            });
+          onPressCloseDialog: function(){
+            // note: We don't need to chain to the pDialog promise, since this event-handler
+            // is only called from within the loaded dialog itself.
+            this.byId("helloDialog").close();       
+            // this.pDialog.then(oDialog => oDialog.close());
             // Toast: The dialog has been closed
             this.getView().byId("toastDialogClose").show();
           }
