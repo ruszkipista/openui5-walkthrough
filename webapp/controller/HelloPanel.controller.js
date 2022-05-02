ce61d9815f40e92847a3096d136358c420d953e9
ruszkipista <oi5672@gmail.com>
2022-05-02 21:31:54 +0100

    Step 16: Dialogs and Fragments - add Toast in view

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define(
     ["sap/ui/core/mvc/Controller",
-     "sap/m/MessageToast"],
+     "sap/m/MessageToast",
+    ],
     function (Controller, MessageToast) {
       "use strict";
 
       return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
-          onShowHello: function(){
+          onShowToast: function(){
             // read msg from i18n model
             let oBundle = this.getView().getModel("i18n").getResourceBundle();
             let sRecipient = this.getView().getModel().getProperty("/recipient/name");
@@ -14,5 +15,26 @@ sap.ui.define(
             MessageToast.show(sMsg);
           },
 
+          onOpenDialog: function(){
+            // create dialog lazily
+            if (!this.pDialog) {
+              this.pDialog = this.loadFragment({
+                name: "sap.ui.demo.walkthrough.view.HelloDialog"
+              });
+            }
+            this.pDialog
+            .then(function(oDialog) {
+              oDialog.open();
+            });
+          },
+
+          onPressDialogCancelButton: function(){
+            this.pDialog
+            .then(function(oDialog) {
+              oDialog.close();
+            });
+            // Toast: The dialog has been closed
+            this.getView().byId("toastDialogClose").show();
+          }
       });
  });
