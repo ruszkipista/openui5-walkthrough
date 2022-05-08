514b631e292cbdc654c28a3b31c442a167cac0f8
ruszkipista <oi5672@gmail.com>
2022-05-08 22:02:23 +0100

    Step 23: Filtering - add search field

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -2,8 +2,10 @@ sap.ui.define([
     'sap/ui/core/mvc/Controller',
     'sap/m/MessageToast',
     "../model/formatters",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
     ], 
-    function(Controller, MessageToast, formatters) {
+    function(Controller, MessageToast, formatters, Filter, FilterOperator) {
         "use strict";
         return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList.List", {
 
@@ -14,6 +16,22 @@ sap.ui.define([
                 let oBundle = this.getView().getModel("i18n").getResourceBundle();
                 let sMsg = oBundle.getText("app.list.invoices.toast", [sItemTitle]);        
                 MessageToast.show(sMsg);
+            },
+
+            onChangeInvoicesFilter : function (oEvent) {
+    
+                // build filter array
+                let aFilter = [];
+                let sQuery = oEvent.getParameter("query");
+                if (sQuery) {
+                    // filter operator Contains is not case-sensitive
+                    aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+                }
+    
+                // filter binding
+                let oList = this.byId("invoiceList");
+                let oBinding = oList.getBinding("items");
+                oBinding.filter(aFilter);
             }
         });
 
